2fc197c2cc2c44372c8c6550f12893bfb1fb3d1f
fatemeh <fmmajd452@gmail.com>
2020-02-22 16:03:16 +0330

    removed a complicating test

--- db_test.go
+++ db_test.go
@@ -5,13 +5,6 @@ import (
 	"testing"
 )
 
-func TestDbNotInitializedBeforeInitCall(t *testing.T) {
-	db := DB
-	if db.connection != nil {
-		t.Error("db is initialized without the call to InitDB function")
-	}
-}
-
 func TestInitDB(t *testing.T) {
 	InitDB("test", "user", "pass", "localhost", nil, pgx.LogLevelDebug)
 	if DB.connection == nil {
